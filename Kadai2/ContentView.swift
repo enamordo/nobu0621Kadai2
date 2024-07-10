22856ca9088c0c887f792363867195122f82cdaf
Atsunobu Yuge <yugechan666@gmail.com>
2024-07-10 22:57:58 +0900

    Initial Commit

---
+++ Kadai2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kadai2
+//
+//  Created by 弓削　敦信 on 2024/07/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kadai2/Kadai2App.swift
@@ -0,0 +1,17 @@
+//
+//  Kadai2App.swift
+//  Kadai2
+//
+//  Created by 弓削　敦信 on 2024/07/10.
+//
+
+import SwiftUI
+
+@main
+struct Kadai2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
