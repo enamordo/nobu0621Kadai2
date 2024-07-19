ebd16bfba9442356f3eea65ccb07cbec35d7e08c
Atsunobu Yuge <yugechan666@gmail.com>
2024-07-20 00:38:34 +0900

    akioさんのFBを反映

--- Kadai2/ContentView.swift
+++ Kadai2/ContentView.swift
@@ -12,14 +12,14 @@ struct ContentView: View {
     @State private var inputNumber2: String = ""
     @State private var result: String = "Label"
     @State private var selectedSign = CalculationSigns.plus
-    
+
     var body: some View {
         VStack {
             TextField("", text: $inputNumber1)
             TextField("", text: $inputNumber2)
             Picker("calculationSigns", selection: $selectedSign) {
                 ForEach(CalculationSigns.allCases) {
-                    Text($0.rawValue).tag($0)
+                    Text($0.displayText).tag($0)
                 }
             }
             .pickerStyle(.segmented)
@@ -35,11 +35,11 @@ struct ContentView: View {
                 case .multiplied:
                     result = String(number1 * number2)
                 case .divided:
-                    if number2 == 0 {
+                    guard number2 != 0 else {
                         result = "割る数には0以外を入力して下さい"
-                    } else {
-                        result = String(number1 / number2)
+                        return
                     }
+                    result = String(number1 / number2)
                 }
             }
             HStack {
@@ -53,13 +53,25 @@ struct ContentView: View {
     }
 }
 
-private enum CalculationSigns: String, CaseIterable, Identifiable {
-    case plus = "+"
-    case minus = "-"
-    case multiplied = "×"
-    case divided = "÷"
+private enum CalculationSigns: CaseIterable, Identifiable{
+    case plus
+    case minus
+    case multiplied
+    case divided
 
-    var id: String { rawValue }
+    var id: Self{ self }
+    var displayText: String {
+        switch self {
+        case .plus:
+            return "+"
+        case .minus:
+            return "-"
+        case .multiplied:
+            return "×"
+        case .divided:
+            return "÷"
+        }
+    }
 }
 
 #Preview {
